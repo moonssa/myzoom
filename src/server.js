4650514960ac27af1c0ff105f57325d23c481492
moonk <mkjoehsb@gmail.com>
2022-03-06 16:58:45 +0900

    handlelisten

--- src/server.js
+++ src/server.js
@@ -8,5 +8,5 @@ app.use("/public", express.static(__dirname+"/public"));
 app.get("/", (req,res) => res.render("home"));
 app.get("/*",(req, res) => res.redirect("/"));
 
-
-app.listen(3000);
+const handleListen = () => console.log(`listening on http://localhost:3000`);
+app.listen(3000, handleListen);
