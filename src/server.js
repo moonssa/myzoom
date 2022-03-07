cd9fdf3546d190f3a35aa74321686ba415cf1464
moonk <mkjoehsb@gmail.com>
2022-03-07 10:34:00 +0900

    chatting

--- src/server.js
+++ src/server.js
@@ -2,27 +2,27 @@ import express from "express";
 import http from "http";
 import WebSocket from "ws";
 
-const app= express();
+const app = express();
 app.set("view engine", "pug");
-app.set("views", __dirname+"/views");
-app.use("/public", express.static(__dirname+"/public"));
+app.set("views", __dirname + "/views");
+app.use("/public", express.static(__dirname + "/public"));
 
-app.get("/", (req,res) => res.render("home"));
-app.get("/*",(req, res) => res.redirect("/"));
+app.get("/", (req, res) => res.render("home"));
+app.get("/*", (req, res) => res.redirect("/"));
 
 const handleListen = () => console.log(`listening on http://localhost:3000`);
 
-const server= http.createServer(app);
-const wss = new WebSocket.Server({server});
-
+const server = http.createServer(app);
+const wss = new WebSocket.Server({ server });
+const sockets = [];
 
 wss.on("connection", (socket) => {
-    console.log("Connected to Browser  ✅ ");
+  sockets.push(socket);
+  console.log("Connected to Browser  ✅ ");
 
-    socket.on("close", () => console.log("Disconnected from Browser ❌"));
-    socket.on("message", (message) => {
-        console.log(message.toString("utf8"));
-    });
-    socket.send("hello socket!!!");
+  socket.on("close", () => console.log("Disconnected from Browser ❌"));
+  socket.on("message", (message) => {
+    sockets.forEach((aSocket) => aSocket.send(message.toString()));
+  });
 });
 server.listen(3000, handleListen);
