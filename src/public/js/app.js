b32f06d8dfb5a519c56a62ac3e5ff55a6be25974
moonk <mkjoehsb@gmail.com>
2022-03-09 23:38:35 +0900

    room message

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -13,11 +13,23 @@ function addMessage(msg) {
   ul.appendChild(li);
 }
 
+function handleMessageSubmit(event) {
+  event.preventDefault();
+  const input = room.querySelector("input");
+  const value = input.value;
+  socket.emit("new_message", value, roomName, ()=> {
+      addMessage(`You: ${value}`);
+  });
+  input.value = "";
+}
+
 function showRoom() {
   welcome.hidden = true;
   room.hidden = false;
   h3 = room.querySelector("h3");
   h3.innerText = `Room :  ${roomName}`;
+  const form = room.querySelector("form");
+  form.addEventListener("submit", handleMessageSubmit);
 }
 
 function handleRoomSubmit(event) {
@@ -29,6 +41,14 @@ function handleRoomSubmit(event) {
 }
 
 form.addEventListener("submit", handleRoomSubmit);
+
 socket.on("welcome", () => {
   addMessage("Someone joined");
 });
+
+socket.on("bye", () => {
+  addMessage("someone left ㅠㅠ");
+});
+
+socket.on("new_message", addMessage);
+
