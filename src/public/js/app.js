0ec30f36ddeed65dff78d55ea0bbfaccccef29d8
moonk <mkjoehsb@gmail.com>
2022-03-09 20:41:14 +0900

    user join
    socket.to 사용

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -6,19 +6,29 @@ const room = document.getElementById("room");
 room.hidden = true;
 let roomName;
 
+function addMessage(msg) {
+  const ul = room.querySelector("ul");
+  const li = document.createElement("li");
+  li.innerText = msg;
+  ul.appendChild(li);
+}
+
 function showRoom() {
-    welcome.hidden = true;
-    room.hidden = false;
-    h3 = room.querySelector("h3");
-    h3.innerText = `Room :  ${roomName}`;
+  welcome.hidden = true;
+  room.hidden = false;
+  h3 = room.querySelector("h3");
+  h3.innerText = `Room :  ${roomName}`;
 }
 
 function handleRoomSubmit(event) {
   event.preventDefault();
   const input = form.querySelector("input");
-  socket.emit("enter_room",  input.value , showRoom);
+  socket.emit("enter_room", input.value, showRoom);
   roomName = input.value;
   input.value = "";
-};
+}
 
 form.addEventListener("submit", handleRoomSubmit);
+socket.on("welcome", () => {
+  addMessage("Someone joined");
+});
